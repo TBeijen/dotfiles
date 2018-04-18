5e706350dc2d338a7878a23c1be4366df6873919
Tibo Beijen <tibo.beijen@sanoma.com>
2018-04-18 11:13:17 +0200

    Added AWS scripts

--- common/aws_scripts.sh
+++ common/aws_scripts.sh
@@ -1,4 +1,6 @@
-# Usage: aws-login <Google_Authenticator_Code>
+# Login using MFA, acquiring neccessary permisisons
+#
+# Usage: aws_login <Google_Authenticator_Code>
 aws_login() {
   MFA_ARN=$(aws --profile ${AWS_PROFILE:-default} --output text iam list-mfa-devices --query 'MFADevices[].SerialNumber')
   TOKEN_CODE=$1
@@ -7,3 +9,96 @@ aws_login() {
   export AWS_SECRET_ACCESS_KEY=$(echo $CREDENTIALS | jq -r '.SecretAccessKey')
   export AWS_SESSION_TOKEN=$(echo $CREDENTIALS | jq -r '.SessionToken')
 }
+
+# Lookup EC2 instance ip.
+#
+# Usage: 
+#   aws_ip <Instance_Id>
+#   ssh $(aws_ip <Instance_Id>)
+aws_ip() {
+  INSTANCE_ID=$1
+  aws --output text ec2 describe-instances --instance-id $INSTANCE_ID --query 'Reservations[].Instances[].PrivateIpAddress'
+}
+
+# List instances of auto scaling group.
+# Optionally sets desired capacity.
+#
+# Usage: 
+#   aws_asg_instances <ASG_Name>
+#   aws_asg_instances <ASG_Name> <Number_Of_Instances>
+aws_asg_instances() {
+  ASG_NAME=$1
+  DESIRED_CAPACITY=$2
+  aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $ASG_NAME --query 'AutoScalingGroups[].Instances[]'
+  if [[ -n $DESIRED_CAPACITY ]]; then
+    aws autoscaling set-desired-capacity --auto-scaling-group-name $ASG_NAME --desired-capacity $DESIRED_CAPACITY && \
+    echo "Changed desired capacity to $DESIRED_CAPACITY"
+  fi
+}
+
+# Add instance to autoscaling group. Increases desired capacity.
+#
+# Usage:
+#   aws_asg_add <ASG_Name>
+aws_asg_add() {
+  ASG_NAME=$1
+  CURRENT_CAPACITY=$(aws --output text autoscaling describe-auto-scaling-groups --auto-scaling-group-names $ASG_NAME --query 'AutoScalingGroups[].DesiredCapacity')
+  DESIRED_CAPACITY=$((CURRENT_CAPACITY+1))
+  aws autoscaling set-desired-capacity --auto-scaling-group-name $ASG_NAME --desired-capacity $DESIRED_CAPACITY && \
+    echo "Changed desired capacity to $DESIRED_CAPACITY"
+}
+
+# Show auto scaling group health
+#
+# Usage:
+#   aws_asg_health <ASG_Name>
+aws_asg_health() {
+  ASG_NAME=$1
+  aws elbv2 describe-target-health --target-group-arn $(aws --output text autoscaling describe-auto-scaling-groups --auto-scaling-group-names $ASG_NAME --query 'AutoScalingGroups[].TargetGroupARNs[]') --query 'TargetHealthDescriptions[].[Target.Id,TargetHealth.State]'
+}
+
+# Replace instance in autoscaling group. Preserves desired capacity.
+#
+# Usage:
+#   aws_asg_replace <Instance_Id>
+aws_asg_replace() {
+  INSTANCE_ID=$1
+  aws autoscaling terminate-instance-in-auto-scaling-group --instance-id $INSTANCE_ID --no-should-decrement-desired-capacity
+}
+
+# Terminate instance in autoscaling group. Decreases desired capacity.
+#
+# Usage:
+#   aws_asg_terminate <Instance_Id>
+aws_asg_terminate() {
+  INSTANCE_ID=$1
+  aws autoscaling terminate-instance-in-auto-scaling-group --instance-id $INSTANCE_ID --should-decrement-desired-capacity
+}
+
+# Set AWS profile
+# 
+# Usage:
+#   aws_set_profile <Config_Profile>
+#   aws_set_profile sandbox
+aws_set_profile() {
+  VALID_PROFILES=($(grep -E "^\[.+\]$" ~/.aws/config | sed -e 's!\[!!g' -e 's!profile !!g' -e 's!\]!!g'))
+  for ((i = 0; i < ${#VALID_PROFILES[@]}; i++)); do
+    if [[ ${VALID_PROFILES[$i]} = $1 ]]; then
+        break
+    fi
+  done
+  if ((i == ${#VALID_PROFILES[@]})); then
+    echo -e "${red}Unknown profile, abort${reset}"
+  else
+    export AWS_PROFILE=$1
+  fi
+}
+
+# List available AWS profiles
+# 
+# Usage:
+#   aws_get_profiles
+aws_get_profiles() {
+    grep -E "^\[.+\]$" ~/.aws/config | sed -e 's!\[!!g' -e 's!profile !!g' -e 's!\]!!g'
+}
+complete -W "$(aws_get_profiles)" aws_set_profile
