fab82ff5c9dd24d926deb7631c0f8e072e7d304b
Tibo Beijen <tibo.beijen@sanoma.com>
2018-08-10 09:05:18 +0200

    Falling back to local minikube if AWS profile doesn't have a k8s cluster.

--- common/scripts.sh
+++ common/scripts.sh
@@ -10,13 +10,25 @@ set_workspace() {
   ACCOUNT=$1
   ROLE=$2
   CONTEXT=$ACCOUNT
+  AWSPROF=$ACCOUNT
   if [[ $ROLE ]]; then
   	CONTEXT=$ACCOUNT-$ROLE
+    AWSPROF=$ACCOUNT-$ROLE
   fi
+  # Kops
   export KOPS_STATE_STORE=s3://${ACCOUNT}-kops-state
   export CLUSTER_NAME=${ACCOUNT}.k8s.cloud.sanoma.com
+  # AWS
   export AWS_SDK_LOAD_CONFIG=1
-  export AWS_PROFILE=${ACCOUNT}
-  export KUBECONFIG=$HOME/.kube/${ACCOUNT}.config
-  kubectl config use-context ${CONTEXT}
+  export AWS_PROFILE=${AWSPROF}
+  # Kube
+  KUBECONFIG=$HOME/.kube/${ACCOUNT}.config
+  if [ -f $KUBECONFIG ]; then
+    export KUBECONFIG=$KUBECONFIG
+    kubectl config use-context ${CONTEXT}
+  else
+    # default = local minikube
+    export KUBECONFIG=$HOME/.kube/default.config
+    kubectl config use-context minikube
+  fi
 }
