c048b40d9ab1d8e74df7d217fc915002d497e393
Tibo Beijen <tibobeijen@gmail.com>
2019-08-10 21:00:48 +0200

    Added function to clean up merged local and remote git branches.

--- common/scripts.sh
+++ common/scripts.sh
@@ -26,6 +26,52 @@ pyclean () {
     find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
 }
 
+
+
+# In-depth info about deleting branches: https://stackoverflow.com/questions/2003505/how-do-i-delete-a-git-branch-locally-and-remotely
+git_merged() {
+  _show_help "$(cat <<-HELP
+Show or remove merged branches, either local or remote, from the repository the local dir is in.
+
+Usage:
+  git_merged show|delete local|remote|all
+
+  git_merged show all
+  
+  # nothing unusual there?
+  # (Or, if you have other intends, run the output of the above command through xargs...)
+
+  git_merged delete all
+HELP
+  )" 1 "$@" || return 0
+  set -e
+
+  # Script constants
+  BRANCH_WHITELIST="(\*|master|develop)"
+  REMOTES="origin"  # Add more remotes here as space-separated list
+
+  # local
+  if [[ $2 == "local" || $2 == "all" ]]; then
+    for branch in $(git branch --merged master | egrep -v "$BRANCH_WHITELIST"); do
+      if [[ $1 == "show" ]]; then echo "${branch}"; fi
+      if [[ $1 == "delete" ]]; then git branch -d "${branch}"; fi
+    done
+  fi
+
+  # remotes
+  if [[ $2 == "remote" || $2 == "all" ]]; then
+    for remote in "$REMOTES"; do
+      git fetch "$remote" --prune
+      for branch in $(git branch -r --merged master | grep "$remote" | egrep -v "$BRANCH_WHITELIST"); do
+        if [[ $1 == "show" ]]; then echo "${branch}"; fi
+        #  '#*/' part removes the preceding 'origin/' part from the branch name
+        if [[ $1 == "delete" ]]; then git push "${remote}" --delete "${branch#*/}"; fi
+      done
+    done
+  fi
+}
+
+
 set_workspace() {
   _show_help "$(cat <<-HELP
 Consistently switch configuration of: AWS, Kubernetes, Kops 
