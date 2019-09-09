e91605016bd43a9ec38ea7ee81896177d90b3260
Tibo Beijen <tibobeijen@gmail.com>
2019-09-09 10:39:12 +0200

    Added kube_get_sa_token

--- common/kube_scripts.sh
+++ common/kube_scripts.sh
@@ -10,6 +10,25 @@ HELP
 }
 
 
+kube_get_sa_token() {
+  _show_help "$(cat <<-HELP
+Copies token for service account in given namespace to clipboard.
+
+Usage:
+  kube_get_sa_token <namespace> <sa_name>
+
+HELP
+  )" 0 "$@" || return 0 
+
+  NAMESPACE=$1
+  SA_NAME=$2
+
+  set -o pipefail
+  kubectl -n $NAMESPACE get secrets $(kubectl -n $NAMESPACE get sa $SA_NAME -o json | jq -Mr '.secrets[].name') -o json | jq -Mr '.data.token' | base64 -D | pbcopy && echo "Token copied to clipboard"
+}
+
+
+
 kube_port_forward() {
   _show_help "$(cat <<-HELP
 Sets up port forwarding to a pod, using labels as selector
