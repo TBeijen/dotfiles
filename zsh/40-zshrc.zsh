e375ebe03a028d185be64ad66d37fbcaf0895da7
Tibo Beijen <tibo.beijen@persgroep.net>
2023-05-14 08:19:57 +0200

    Added some additional zshrc

---
+++ zsh/40-zshrc.zsh
@@ -0,0 +1,13 @@
+# Even with ssh config stating UseKeyChain, somehow password prompt keeps appearing
+# This seems to work.
+# See: https://www.cyberciti.biz/faq/howto-fix-macos-keeps-asking-my-ssh-passphrase-since-i-updated-to-sierra/
+ssh-add -A 2>/dev/null
+
+# Adding path
+PATH="$HOME/.local/bin/:$PATH"
+
+# nvm
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
+  
