ac7b7f149f426bc84a38d3fd45bed8d46a244c3b
Tibo Beijen <tibobeijen@gmail.com>
2023-09-06 17:25:57 +0200

    Tweaks

--- zsh/99-zshrc.zsh
+++ zsh/99-zshrc.zsh
@@ -10,5 +10,6 @@ PATH="$HOME/.local/bin/:$PATH"
 export NVM_DIR="$HOME/.nvm"
   [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
+
+export GPG_TTY=$(tty)
   
-zscaler_bundle_on
