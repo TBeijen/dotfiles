521893974f8ba21abdd731cac9002fa281f5095b
Tibo Beijen <tibobeijen@gmail.com>
2023-09-24 14:39:41 +0200

    Adapted nvm to new brew location

--- zsh/99-zshrc.zsh
+++ zsh/99-zshrc.zsh
@@ -8,7 +8,7 @@ PATH="$HOME/.local/bin/:$PATH:$HOME/Library/Python/3.9/bin"
 
 # nvm
 export NVM_DIR="$HOME/.nvm"
-  [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
+  [ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm"
 
 export GPG_TTY=$(tty)
