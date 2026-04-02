43eafed2b16aaf9c6c424ec6e43d7edb1b7f14fb
Tibo Beijen <tibobeijen@gmail.com>
2026-04-02 14:45:20 +0200

    Updated network scheiss script comments

--- common/network_scheiss.sh
+++ common/network_scheiss.sh
@@ -1,16 +1,30 @@
 # Create this bundle by combining a suitable root certificate bundle with the zscaler root certificate
 # 
 # Using brew, probably this bundle exists:
-#    openssl storeutl -noout -text -certs /usr/local/etc/ca-certificates/cert.pem
+#    openssl storeutl -noout -text -certs /opt/homebrew/etc/ca-certificates/cert.pem
 # From zscaler, probably this certificate exists:
 #    openssl storeutl -noout -text -certs ~/.zcli/zscaler_root.pem
 #
 # Alternatively, locate in OSX Keychain, export zscaler as pem (System) and all certs into single pem (System roots)
 #
-# Concat pems into single bundle:
+# Brew bundle has post-install hook that adds Keychain system roots to the bundle.
+# System roots might already has been provisioned so it's possible ca-certificates is already
+# completed with zscaler root cert. To verify:
+#
+#   openssl storeutl -noout -text /opt/homebrew/etc/ca-certificates/cert.pem 2>/dev/null | grep -A2 "Zscaler"
+# 
+# To trigger re-creation of the ca-certificates bundle:
+#
+#   brew reinstall ca-certificates
+#
+# Concat pems into single bundle (if needed):
 #
 #   cat /usr/local/etc/ca-certificates/cert.pem > ~/.zscaler/bundle_combined.pem
 #   cat ~/.zcli/zscaler_root.pem >> ~/.zscaler/bundle_combined.pem
+#
+# Else, simply copy ca-certificates bundle to the combined bundle location:
+#
+#   cp /opt/homebrew/etc/ca-certificates/cert.pem ~/.zscaler/bundle
 #
 ZSCALER_COMBINED_BUNDLE=~/.zscaler/bundle_combined.pem
 
